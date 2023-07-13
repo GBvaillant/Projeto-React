82c3037cf833365d41ffa98593d22586a3e3af38
GBvaillant <vaillantgabriel@gmail.com>
2023-07-13 13:33:35 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+
+import Container from "./components/layout/Container";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+          <Route exact path="/projects" element={<Projects />} />
+        </Routes>
+      </Container>
+
+      <Footer />
+    </Router>
+  )
+}
+export default App;
+
