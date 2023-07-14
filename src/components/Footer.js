d806ccc79c2ef8680c3664c4c465a16488985233
GBvaillant <vaillantgabriel@gmail.com>
2023-07-14 18:20:55 -0300

    Mod: add componentes + layouts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
                 </li>
             </ul>
             <p className={styles.copy_right} ><span>Costs </span> 
-            &copy; 2022</p>
+            &copy; 2023</p>
         </footer>
     )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,20 @@
 import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import Message from "../layout/Message"
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
+import Loading from '../layout/Loading'
+
+
+import styles from './Projects.module.css'
 
 function Projects() {
+
+    const [projects, setProjects] = useState([])
+    const [removeLoadind, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -10,11 +22,68 @@ function Projects() {
         message = location.state.message
     }
 
+    useEffect(() => {
+
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProjects(data)
+                setRemoveLoading(true)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProjects(projects.filter((project) => project.id !== id))
+                setProjectMessage('Projeto removido com sucesso')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+
+            </div>
             {message && <Message msg={message} type='success' />}
+            {projectMessage && <Message msg={projectMessage} type='success' />}
+            <Container customClass='start'>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project?.category?.name}
+                            key={project.id}
+                            handleRemove={removeProject}
+                        />
+                    ))}
+                {!removeLoadind && <Loading />}
+                {removeLoadind && projects.length === 0 && (
+                    <p>Não há projetos  cadastrados !!</p>
+                )}
+            </Container>
         </div>
     )
 }
